d484664449215e9df996cb3d504b05e01c49a05d
mehul4mak <mehul4mak@gmail.com>
2025-10-19 15:44:58 +0530

    moved main.py to src

---
+++ src/main.py
@@ -0,0 +1,15 @@
+"""
+BDD Dataset EDA
+
+Author: Mehul Kumawat
+Date: 19.10.2025
+"""
+
+
+def main():
+    """Main Function"""
+    print("Hello from bdd-od!")
+
+
+if __name__ == "__main__":
+    main()
